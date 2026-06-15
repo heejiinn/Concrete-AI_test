163483dd9b736120f889f7945190d6cefebf0535
heejiinn <6642khj@gmail.com>
2026-06-15 13:47:48 +0900

    app.py v9

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ if not st.session_state.authenticated:
     pwd = st.text_input(
         "비밀번호를 입력하세요",
         type="password",
-        key="password_input"
-    )
+        key="password_input")
 
     if st.button("로그인"):
         if pwd == PASSWORD:
@@ -40,6 +39,7 @@ st.write("입력값을 넣으면 화재 후 압축강도를 예측합니다.")
 # 기본 입력란
 temperature = st.number_input("Temperature (°C)", value=600.0)
 fc_28 = st.number_input("FC_28 (MPa)", value=40.0)
+fc_90 = st.number_input("FC_90 (MPa)", value=None)
 cement = st.number_input("Cement (kg/m³)", value=400.0)
 water = st.number_input("Water (kg/m³)", value=180.0)
 
@@ -66,12 +66,11 @@ else:
         "Cooling period (days)",
         min_value=1.0,
         value=1.0,
-        step=1.0
-    )
+        step=1.0)
 
 st.write("Fiber information")
 
-st.write("동일한 종류의 섬유를 여러 길이로 사용한 경우, 함량은 합산하여 입력하고,길이는 함량 가중평균 길이를 입력하세요.")
+st.write("동일한 종류의 섬유를 여러 길이로 사용한 경우, 함량은 합산하여 입력하고, 길이는 함량 가중평균 길이를 입력하세요.")
 
 fiber_types = ["None", "Steel", "PVA", "PP", "Glass", "Basalt"]
 
@@ -92,30 +91,26 @@ for i in range(st.session_state.fiber_count):
     fiber_type = st.selectbox(
         f"Fiber {i+1} type",
         fiber_types,
-        key=f"fiber_type_{i}"
-    )
+        key=f"fiber_type_{i}")
 
     fiber_content = st.number_input(
         f"Fiber {i+1} content (kg/m³)",
         min_value=0.0,
         value=0.0,
         step=0.1,
-        key=f"fiber_content_{i}"
-    )
+        key=f"fiber_content_{i}")
 
     fiber_length = st.number_input(
         f"Fiber {i+1} length (mm)",
         min_value=0.0,
         value=0.0,
         step=0.1,
-        key=f"fiber_length_{i}"
-    )
+        key=f"fiber_length_{i}")
 
     fibers.append({
         "type": fiber_type,
         "content": fiber_content,
-        "length": fiber_length
-    })
+        "length": fiber_length})
 
 if st.button("예측하기"):
     # 학습 때 사용한 변수 구조와 동일한 빈 데이터 생성
@@ -125,6 +120,12 @@ if st.button("예측하기"):
     # 입력한 값 반영
     input_df.loc[0, "Temperature"] = temperature
     input_df.loc[0, "FC_28"] = fc_28
+
+    if fc_90 == "":
+        input_df.loc[0, "FC_90"] = np.nan
+    else:
+        input_df.loc[0, "FC_90"] = float(fc_90)
+
     input_df.loc[0, "W/C"] = wc
     input_df.loc[0, "Cement"] = cement
     input_df.loc[0, "Water"] = water
@@ -148,8 +149,7 @@ if st.button("예측하기"):
         "PVA_fibre", "PVA_fibre_length",
         "PP_fibre", "PP_fibre_length",
         "Glass_fibre", "Glass_fibre_length",
-        "Basalt_fibre", "Basalt_fibre_length"
-    ]
+        "Basalt_fibre", "Basalt_fibre_length"]
 
     for col in fiber_columns:
         input_df.loc[0, col] = 0
